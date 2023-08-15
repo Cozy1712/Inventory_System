d541d25a4494dc6754f638f4223176770e9ad8b0
cozy1712 <kolawolekabiru89@gmail.com>
2023-08-15 13:34:06 +0100

    added user/form.py

---
+++ user/form.py
@@ -0,0 +1,14 @@
+from django import forms 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+#  creating an email field to the usercreation form
+class CreateUserForm(UserCreationForm):
+    firstname = forms.CharField()
+    lastname = forms.CharField()
+    email = forms.EmailField()
+    
+    
+    class Meta:
+        model = User
+        fields = ['firstname', 'lastname', 'username', 'email', 'password1', 'password2' ]
--- user/views.py
+++ user/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from .form import CreateUserForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
+        form = CreateUserForm(request.POST) #the user detail
+        if form.is_valid(): #check for the form validation
             form.save()
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
+        
     context = {
         'form' : form,
     }
