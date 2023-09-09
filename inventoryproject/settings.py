1b4a02efb3a898246c742829575550563900dfc0
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-09 11:54:12 +0100

    Added Reset_Password

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,7 +160,7 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'djangocozy@gmail.com'
-EMAIL_HOST_PASSWORD = 'ujecsbcyodvpewxa'
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASS')
 EMAIL_PORT = 587
 
