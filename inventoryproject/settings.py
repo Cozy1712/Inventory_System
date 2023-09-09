27613edc591cd2d9064416e5bbc62c97b20add6c
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-09 13:18:15 +0100

    update rest_password

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+from dotenv import load_dotenv
+import os
 
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,7 +160,7 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_PASS')
+EMAIL_HOST_USER = 'djangocozy@gmail.com'
+EMAIL_HOST_PASSWORD = 'yktpwxrstidlfgnd'
 EMAIL_PORT = 587
 
