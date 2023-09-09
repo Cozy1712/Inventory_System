0d33efa79da23ae7d887e92057ad2ea1a2e66606
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-09 08:07:28 +0100

    first commit

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -149,3 +149,12 @@ LOGIN_URL = 'user-login'
 MEDIA_ROOT = (BASE_DIR / 'media')
 
 MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djangocozy@gmail.com'
+EMAIL_HOST_PASSWORD = 'ujecsbcyodvpewxa'
+EMAIL_PORT = 587
+
